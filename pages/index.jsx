199820a97f57739ac8afba4fd9d64e7670223cbd
alex <alexis.diavatis@gmail.com>
2016-05-21 13:16:44 +0300

    add something like a home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import DocumentTitle from 'react-document-title'
 import { config } from 'config'
 import { prefixLink } from 'gatsby-helpers'
 import { Link } from 'react-router'
+import FlatButton from 'material-ui/FlatButton'
 
 
 const appIndex = React.createClass({
@@ -18,10 +19,33 @@ const appIndex = React.createClass({
     return (
       <DocumentTitle title={`${appIndex.metadata().title} | ${config.siteTitle}`}>
         <div>
-          <h1>Hello,</h1>
-          <p>
-          There is nothing to see here yet! Please jump on {<Link to={prefixLink('/docs/')} >DOCS</Link>}
-          </p>
+          <div
+            style={{
+              background: '#00838F',
+              position: 'absolute',
+              width: '100%',
+              height: '500px',
+              left: '0px',
+              zIndex: '-1000',
+              top: '56px',
+            }}
+          />
+          <div className="hero-center">
+            <div>
+              <FlatButton
+                label="DOCS"
+                containerElement={<Link to={prefixLink('/docs/')} />}
+              />
+              <iframe
+                className="github-btn"
+                src="https://ghbtns.com/github-btn.html?user=alex285&repo=myGNOME&type=star&count=true&"
+                frameBorder="0"
+                scrolling="0"
+                width="170px"
+                height="20px"
+              />
+            </div>
+          </div>
         </div>
       </DocumentTitle>
     )
